47b5f2a373272090a8615c18b90479074fe8dc69
Marcus Chui <marcuschui@icloud.com>
2019-01-24 18:22:28 +0800

    setup app for heroku again

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp',
+  process.env.PROD_MONGODB || 'mongodb://localhost:27017/TodoApp',
   { useNewUrlParser: true }
 );
 
+console.log(process.env.MONGODB);
 module.exports = { mongoose };
