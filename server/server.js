645a3753e96f5b96f182213b9a700daf45a4bf4a
Marcus Chui <marcuschui@icloud.com>
2019-01-25 15:35:55 +0800

    add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -45,15 +45,35 @@ app.get('/todos/:id', (req, res) => {
     return res.status(404).send();
   }
 
-  Todo.findById(id).then(
-    todo => {
+  Todo.findById(id)
+    .then(todo => {
       if (!todo) {
         return res.status(404).send();
       }
       res.send({ todo });
-    },
-    err => res.status(400).send(err)
-  );
+    })
+    .catch(err => {
+      res.status(400).send();
+    });
+});
+
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.send({ todo });
+    })
+    .catch(err => {
+      res.status(400).send();
+    });
 });
 
 app.listen(port, () => {
