2c958b319f438e1f26ee69999d8df43e7cf4f171
Marcus Chui <marcuschui@icloud.com>
2019-01-28 18:26:47 +0800

    add setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(
-  process.env.PROD_MONGODB || 'mongodb://localhost:27017/TodoApp',
+  process.env.MONGODB_URI,
   { useNewUrlParser: true }
 );
 
-console.log(process.env.MONGODB);
+// console.log(process.env.MONGODB);
 module.exports = { mongoose };
